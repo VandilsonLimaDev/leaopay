64d888a8d8f0d184fbfa24c8a97fbfe035704647
Vandilson <vandilson@oam.int>
2025-05-21 12:11:54 -0300

    Ajustando responsibidade

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,js,ts,jsx,tsx}', // <- ajusta isso ao seu projeto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
